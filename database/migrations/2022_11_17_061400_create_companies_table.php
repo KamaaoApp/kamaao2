45edfc82df7c0516c1437ded013a255507397898
ganesh chandra <gchandra4727@gmail.com>
2022-11-17 11:51:25 +0530

    added migration

--- database/migrations/2022_11_17_061400_create_companies_table.php
+++ database/migrations/2022_11_17_061400_create_companies_table.php
@@ -12,9 +12,16 @@ class CreateCompaniesTable extends Migration
      * @return void
      */
     public function up()
-    {
+    { 
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('company_legal_name');
+            $table->string('company_popular_name');
+            $table->string('company_url');
+            $table->string('company_logo');
+            $table->string('about_company', 1000);
+            $table->integer('is_enabled')->default(0)->comment("0 is disabled user, 1 is active");
+            $table->softDeletes();
             $table->timestamps();
         });
     }
