4416ca8e4b2ee0b53f9f0d1774d780b92ab9e012
ganesh chandra <gchandra4727@gmail.com>
2022-11-25 12:58:11 +0530

    added user support model , seeder and migration file

---
+++ database/migrations/2022_11_25_072708_create_user_supports_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserSupportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_supports', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_supports');
+    }
+}
