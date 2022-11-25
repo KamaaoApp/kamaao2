cb99b77d2e2c7037194f5a2634c1faf3849e37d7
ganesh chandra <gchandra4727@gmail.com>
2022-11-25 14:41:07 +0530

    updated user support miration file

--- database/migrations/2022_11_25_072708_create_user_supports_table.php
+++ database/migrations/2022_11_25_072708_create_user_supports_table.php
@@ -13,8 +13,19 @@ class CreateUserSupportsTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_supports', function (Blueprint $table) {
+        Schema::create('support_tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('image1')->nullable();
+            $table->string('image2')->nullable();
+            $table->string('has_reviewed')->nullable()->default(0)->comment("0 is no, 1 is yes");
+            $table->integer('is_open')->default(1)->comment("0 is Clesed, 1 is open");
+            $table->string('description', 500)->nullable();
+            $table->bigInteger('user_id')->unsigned()->index()->nullable();
+            $table->foreign('user_id')->references('id')->on('app_user');
+            $table->bigInteger('reviewed_by')->unsigned()->index()->nullable();
+            $table->foreign('reviewed_by')->references('id')->on('users');
+            $table->dateTime('closing_date')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
