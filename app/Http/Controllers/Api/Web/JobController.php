f913dc22929ca995fd1fe57524f011b36a0d9e5e
Rakesh <gchandra4727@gmail.com>
2022-12-09 15:46:45 +0530

    bitbucket to github

--- app/Http/Controllers/Api/Web/JobController.php
+++ app/Http/Controllers/Api/Web/JobController.php
@@ -64,8 +64,7 @@ class JobController extends Controller
             'message'=>'Document Type Updated'
         ]);
     }
-
-     
+    
     public function destroy(Job $job)
     {
         if($job->delete())
